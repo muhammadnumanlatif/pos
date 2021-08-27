b001789ff70484c364ce76967c440a0b49f5c49b
muhammadnumanlatif <muhammadnumanlatif@gmail.com>
2021-08-27 22:50:54 +0500

    3.create models / reqres.in / quicktype.io

--- lib/app/data/models/user_model.dart
+++ lib/app/data/models/user_model.dart
@@ -1,69 +1,78 @@
-class User {
+
+import 'dart:convert';
+
+UserModel userModelFromJson(String str) => UserModel.fromJson(json.decode(str));
+
+String userModelToJson(UserModel data) => json.encode(data.toJson());
+
+class UserModel {
+  UserModel({
+    this.data,
+    this.support,
+  });
+
   Data? data;
   Support? support;
 
-  User({this.data, this.support});
+  factory UserModel.fromJson(Map<String, dynamic> json) => UserModel(
+    data: Data.fromJson(json["data"]),
+    support: Support.fromJson(json["support"]),
+  );
 
-  User.fromJson(Map<String, dynamic> json) {
-    data = json['data'] != null ? Data?.fromJson(json['data']) : null;
-    support = json['support'] != null ? Support?.fromJson(json['support']) : null;
-  }
-
-  Map<String, dynamic> toJson() {
-    final data = <String, dynamic>{};
-    if (data != null) {
-      data['data'] = data.toJson();
-    }
-    if (support != null) {
-      data['support'] = support.toJson();
-    }
-    return data;
-  }
+  Map<String, dynamic> toJson() => {
+    "data": data!.toJson(),
+    "support": support!.toJson(),
+  };
 }
 
 class Data {
+  Data({
+    this.id,
+    this.email,
+    this.firstName,
+    this.lastName,
+    this.avatar,
+  });
+
   int? id;
   String? email;
   String? firstName;
   String? lastName;
   String? avatar;
 
-  Data({this.id, this.email, this.firstName, this.lastName, this.avatar});
+  factory Data.fromJson(Map<String, dynamic> json) => Data(
+    id: json["id"],
+    email: json["email"],
+    firstName: json["first_name"],
+    lastName: json["last_name"],
+    avatar: json["avatar"],
+  );
 
-  Data.fromJson(Map<String, dynamic> json) {
-    id = json['id'];
-    email = json['email'];
-    firstName = json['first_name'];
-    lastName = json['last_name'];
-    avatar = json['avatar'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final data = <String, dynamic>{};
-    data['id'] = id;
-    data['email'] = email;
-    data['first_name'] = firstName;
-    data['last_name'] = lastName;
-    data['avatar'] = avatar;
-    return data;
-  }
+  Map<String, dynamic> toJson() => {
+    "id": id,
+    "email": email,
+    "first_name": firstName,
+    "last_name": lastName,
+    "avatar": avatar,
+  };
 }
 
 class Support {
+  Support({
+    this.url,
+    this.text,
+  });
+
   String? url;
   String? text;
 
-  Support({this.url, this.text});
+  factory Support.fromJson(Map<String, dynamic> json) => Support(
+    url: json["url"],
+    text: json["text"],
+  );
 
-  Support.fromJson(Map<String, dynamic> json) {
-    url = json['url'];
-    text = json['text'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final data = <String, dynamic>{};
-    data['url'] = url;
-    data['text'] = text;
-    return data;
-  }
+  Map<String, dynamic> toJson() => {
+    "url": url,
+    "text": text,
+  };
 }
